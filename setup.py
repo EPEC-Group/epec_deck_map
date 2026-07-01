0d34b34c109604860290668fde8d0f085e2a1493
Paul Bandarian <paul.bandarian@epecgroup.com.au>
2026-07-01 12:17:54 +1000

    Bump package version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='epec-deck-map',
-    version='0.1.0',
+    version='0.2.0',
     description='Custom deck.gl Dash component — direct layer construction, no @deck.gl/json',
     packages=find_packages(),
     package_data={'epec_deck_map': ['epec_deck_map.min.js']},
